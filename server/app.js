0f71074e866081be3572532178007353dfbadf99
Manuel Martin <manuelmad@homeria.com>
2021-10-09 17:57:41 +0200

    fix: CORS problem

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 // Imports
 const express = require('express');
 require('dotenv').config();
+const cors = require("cors");
 const api_products = require('./routes/api_products');
 const api_users = require('./routes/api_users');
 
 // Variables
 const app = express();
 const port = process.env.PORT;
+const corsOptions = {
+    origin: [
+      "https://localhost:process.env.PORT"
+    ]
+  };
 
 // Middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
