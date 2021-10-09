74daf2cecd4f67e573221d2326e3d2a0b5f7eff2
Manuel Martin <manuelmad@homeria.com>
2021-10-09 18:37:22 +0200

    code: front signup register form

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Toast } from "../../hooks/useToast";
+// import { Toast } from "../../hooks/useToast";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
@@ -9,11 +9,11 @@ const Login = () => {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data, e) => {
-    
+    // ! Pendiente de probar
    if (data){
      const payload = {email: data.email, password: data.password};
      (async () => {
-      const response = await axios.post("http://localhost:9000/products", payload);
+      const response = await axios.post("http://localhost:9000/users/login", payload);
       console.log(response);
      })()
    }
@@ -21,13 +21,22 @@ const Login = () => {
 
   return (
     <section className="login">
-      <form className="new-form" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
         <input type="email" placeholder="email" name="email" {...register("email", { required: true })} />
         <input type="password" placeholder="password" name="passw" {...register("passw", { required: true, minLength: 8 })} />
         <button type="submit">Login...</button>
       </form>
       <Link to="/recover" className="login-recover">
-        <p>Remenber password</p>
+        <p>Remember password</p>
+      </Link>
+      <Link to="/google" className="login-recover">
+        <img src="/assets/signin_google.png" alt="" />
+      </Link>
+      <Link to="/github" className="login-recover">
+      <img src="/assets/signin_github.png" alt="" />
+      </Link>
+      <Link to="/signup" className="login-recover">
+        <p>Dont have account? Sign up here</p>
       </Link>
     </section>
   );
--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data, e) => {
+    if (data) {
+      const payload = { 
+        name: data.name,
+        surname: data.surname,
+        email: data.email,
+        password: data.password,
+        address: data.address 
+      };
+
+      (async () => {
+        const response = await axios.post(
+          "http://localhost:9000/users/signup",
+          payload
+        );
+        console.log(response);
+      })();
+    }
+  };
+
+  return (
+    <section className="signup">
+      <form
+        className="signup-form"
+        onSubmit={handleSubmit(onSubmit)}
+        autoComplete="off"
+      >
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          {...register("name", { required: "You must specify name" })}
+        />
+        <input
+          type="text"
+          placeholder="surname"
+          name="surname"
+          {...register("surname", { required: "You must specify surname" })}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          {...register("email", { required: "You must specify a correct email" })}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          {...register("password", { required: true, minLength: 8 })}
+        />
+        <input
+          type="password"
+          placeholder="repeat password"
+          name="password_repeat"
+          {...register("password_repeat", { required: true, minLength: 8 })}
+        />
+        <input
+          type="text"
+          placeholder="address"
+          name="address"
+          {...register("address", { required: "You must specify an address" })}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </section>
+  );
 };
 
 export default Signup;
